2fa26ef4d74334e5ba8fe511dda0cdeeb48ae2c9
R <reshmarmk33@gmail.com>
2022-03-19 18:03:06 +0530

    Development FrontEnd

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from api.models import Task
 
 # Create your views here.
+def dashboard(request):
+    if request.method == 'POST':
+
+        actions = request.POST.keys()
+        if 'create' in actions:
+            if request.POST['Title']:
+                Task.objects.create(task=request.POST['Title'])
+        if 'update' in actions:
+            Task.objects.filter(id=request.POST['update']).update(task=request.POST['Title'])
+        if 'delete' in actions:
+            Task.objects.filter(id=request.POST['delete']).delete()
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'frontend/list.html', context=context)
